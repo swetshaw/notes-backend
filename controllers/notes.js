efd11eeac4badf0b8501cd260fcecfbabaff6152
swetshaw <swt.shw94@gmail.com>
2021-04-06 03:02:07 +0530

    Added user login id and password hashing

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,10 +1,10 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
-notesRouter.get('/', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note.find({}).populate('user', { username:1, name:1 })
+  response.json(notes)
 })
 
 notesRouter.get('/:id', (request, response, next) => {
@@ -19,20 +19,22 @@ notesRouter.get('/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+  //   console.log('user', user);
   const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date()
+    date: new Date(),
+    user: user._id
   })
 
-  note.save()
-    .then(savedNote => {
-      response.json(savedNote)
-    })
-    .catch(error => next(error))
+  const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+  response.json(savedNote)
 })
 
 notesRouter.delete('/:id', (request, response, next) => {
