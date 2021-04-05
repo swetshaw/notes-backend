091bd7e608ceb4336242d53dacec5feec2eba607
swetshaw <swt.shw94@gmail.com>
2021-04-06 02:22:34 +0530

    Notes directory structure

--- models/note.js
+++ models/note.js
@@ -1,35 +1,24 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI;
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  important: Boolean,
+})
 
-console.log("Connecting to", url);
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
-mongoose
-  .connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
-  .then((result) => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("Error connecting to MongoDB", error.message);
-  });
-
-  const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean,
-  });
-  
-  noteSchema.set('toJSON', {
-    transform: (document, returnedObject) =>{
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
-  
-module.exports = mongoose.model("Note", noteSchema);
+module.exports = mongoose.model('Note', noteSchema)
