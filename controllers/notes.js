44b0f996d0a0c4606733f639e8ecce9b98b98981
swetshaw <swt.shw94@gmail.com>
2021-04-06 13:26:07 +0530

    course 4 - user authentication

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,6 +1,15 @@
 const notesRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Note = require('../models/note')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')){
+    return authorization.substring(7)
+  }
+  return null
+}
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({}).populate('user', { username:1, name:1 })
@@ -21,8 +30,14 @@ notesRouter.get('/:id', (request, response, next) => {
 
 notesRouter.post('/', async (request, response) => {
   const body = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
-  const user = await User.findById(body.userId)
+  // const user = await User.findById(body.userId)
   //   console.log('user', user);
   const note = new Note({
     content: body.content,
